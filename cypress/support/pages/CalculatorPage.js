6180ad84b8c6901465706cdf166635996f2edf3e
rahulgit1993 <rahulsregcp@gmail.com>
2024-08-19 14:41:28 +0530

    Commit for code change

--- cypress/support/pages/CalculatorPage.js
+++ cypress/support/pages/CalculatorPage.js
@@ -13,26 +13,11 @@ class CalculatorPage {
     }
     selectInsuranceProduct(Vollversicherung) {
         cy.contains('span', Vollversicherung,{ timeout: 10000 }).click()
-      }
-   
+    }
   
-    /*selectIngressDate(dateToSelect) {
-       Select date from dropdown dynamically
-       cy.get('select[data-cy="ingress-date"]').then($dropdown => {
-       const options = $dropdown.find('option');
-        cy.wrap(options).each(option => {
-        const $option = Cypress.$(option);
-        if ($option.text().trim() === dateToSelect) {
-            cy.wrap($dropdown).select($option.val()).should('have.value', $option.val());
-            return false; // Exit loop once the correct option is found and selected
-          }
-         });
-     });
-    }*/
-  
-           selectIngressDate(dateToSelect) {
-            cy.get('select[data-cy="ingress-date"]').select(dateToSelect).should('have.value', dateToSelect);
-  }
+           selectIngressDate(startDateValue) {
+            cy.get('select[data-cy="ingress-date"]').select(startDateValue).should('have.value', startDateValue);
+    }
         
     continueInsuranceProduct() {
       cy.get('button[data-cy="insurance-product-continue"]').click();
